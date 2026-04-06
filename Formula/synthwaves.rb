269d6dd30bead6f75aa8ab982176dd4a4becf90c
leopolicastro <43428385+leopolicastro@users.noreply.github.com>
2026-04-06 08:42:31 -0400

    Add synthwaves formula v0.1.0

---
+++ Formula/synthwaves.rb
@@ -0,0 +1,17 @@
+class Synthwaves < Formula
+  desc "synthwaves.fm CLI -- manage your music library from the terminal"
+  homepage "https://synthwaves.fm"
+  url "ssh://git@100.98.15.106:23231/synthwaves-cli.git", tag: "v0.1.0"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-X github.com/leo/synthwaves-cli/cmd.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags: ldflags, output: bin/"synthwaves"), "."
+  end
+
+  test do
+    assert_match "SYNTHWAVES", shell_output("#{bin}/synthwaves --help")
+  end
+end
